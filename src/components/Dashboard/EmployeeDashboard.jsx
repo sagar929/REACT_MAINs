031caae6a763db0e7ad33e940b341af4b8ef6c95
sagar929 <sagarshrama130804@gmail.com>
2025-04-26 18:44:53 +0530

    done and dusted

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import Header from '../../other/Header'
-import TaskListNumber from '../../other/TaskListNumber'
-import TaskList from '../TaskList/TaskList'
+import React, { useEffect, useState } from 'react';
+import Header from '../../other/Header';
+import TaskListNumber from '../../other/TaskListNumber';
+import TaskList from '../TaskList/TaskList';
 
-function EmployeeDashboard({data}) {
-  
+function EmployeeDashboard({ data }) {
+  const [employeeData, setEmployeeData] = useState(data); // Start with data
+
+  useEffect(() => {
+    // Always fetch latest employee data from localStorage
+    const employees = JSON.parse(localStorage.getItem('employees')) || [];
+    const updatedEmployee = employees.find(
+      (emp) => emp.email === data.email 
+    );
+    if (updatedEmployee) {
+      setEmployeeData(updatedEmployee);
+    }
+  }, [data]); 
+
   return (
-    <div className='p-10 bg-[#1C1C1C]  h-screen '>
-        
-      <Header data ={data}/>
-      <TaskListNumber data={data}/>
-      <TaskList data={data}/>
+    <div className='p-10 bg-[#1C1C1C] h-screen'>
+      <Header data={employeeData} />
+      <TaskListNumber data={employeeData} />
+      <TaskList data={employeeData} />
     </div>
-  )
+  );
 }
 
-export default EmployeeDashboard
+export default EmployeeDashboard;
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,33 +1,29 @@
-import React, { useEffect } from 'react'
-import AcceptTask from './AcceptTask'
-import NewTask from './NewTask'
-import CompleteTask from './CompleteTask'
-import FailedTask from './FailedTask'
+import React from 'react';
+import AcceptTask from './AcceptTask';
+import NewTask from './NewTask';
+import CompleteTask from './CompleteTask';
+import FailedTask from './FailedTask';
 
-function TaskList({data}) {
-  // useEffect(()=>{
-  //   window.location.reload();
-  // },[])
-
+function TaskList({ data }) {
   return (
     <div id='tasklist' className='h-[55%] overflow-x-auto flex flex-row gap-5 w-full py-5 mt-10'>
-     {data.tasks.map((elem,idx)=>{
-      if(elem.active){
-        return <AcceptTask key={idx} data={elem}/> 
-      }
-      if(elem.NewTask){
-        return <NewTask key={idx} data={elem}/> 
-      }
-      if(elem.completed){
-        return <CompleteTask key={idx} data={elem}/> 
-      }
-      if(elem.failed){
-        return <FailedTask key={idx} data={elem}/> 
-      }
-     })}
-
+      {data.tasks?.map((elem, idx) => {
+        if (elem.active) {
+          return <AcceptTask key={idx} data={elem} />;
+        }
+        if (elem.newTask) {  // fixed small n here
+          return <NewTask key={idx} data={elem} />;
+        }
+        if (elem.completed) {
+          return <CompleteTask key={idx} data={elem} />;
+        }
+        if (elem.failed) {
+          return <FailedTask key={idx} data={elem} />;
+        }
+        return null;
+      })}
     </div>
-  )
+  );
 }
 
-export default TaskList
+export default TaskList;
