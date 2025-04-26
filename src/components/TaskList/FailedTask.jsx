9422b7dbbdf11ce1b445017746e0fae2188f1107
sagar929 <sagarshrama130804@gmail.com>
2025-04-26 18:31:19 +0530

    3:05

--- src/components/TaskList/FailedTask.jsx
+++ src/components/TaskList/FailedTask.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-const FailedTask = () => {
+const FailedTask = ({ data }) => {
   return (
     <div className='h-full flex-shrink-0 w-[300px] bg-blue-500 rounded-xl'>
     <div className='flex justify-between items-center p-3'>
       <h3 className='bg-red-600 px-3 py-1 text-sm rounded text-white'>High</h3>
-      <h4 className='text-base'>20 Feb 2024</h4>
+      <h4 className='text-base'>{data.date}</h4>
     </div>
     <h2 className='mt-5 ml-2 text-2xl font-semibold '>
-        Make a youtube video
+        {data.title}
     </h2>
     <p className= "text-sm ml-2">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat necessitatibus officiis quibusdam suscipit.
+        {data.description}
     </p>
     <div className='mt-5'>
         <button className='w-full rounded-2xl bg-red-900'>Task Failed</button>
